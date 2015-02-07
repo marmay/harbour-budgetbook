4708b4b68455519710a49e124c58917de23cd182
TylerTemp <huangling.exe@gmail.com>
2015-02-07 11:19:03 +0800

    fix the unfinished tag

--- translations/harbour-budgetbook-zh_CN.ts
+++ translations/harbour-budgetbook-zh_CN.ts
@@ -31,243 +31,243 @@
     <name>BillBrowser</name>
     <message>
         <source>Browse Bills</source>
-        <translation type="unfinished">浏览账单</translation>
+        <translation>浏览账单</translation>
     </message>
     <message>
         <source>Delete</source>
-        <translation type="unfinished">删除</translation>
+        <translation>删除</translation>
     </message>
     <message>
         <source>Deleting</source>
-        <translation type="unfinished">正在删除</translation>
+        <translation>正在删除</translation>
     </message>
 </context>
 <context>
     <name>ByCategory</name>
     <message>
         <source>Statistics</source>
-        <translation type="unfinished">统计</translation>
+        <translation>统计</translation>
     </message>
     <message>
         <source>Please add more bills!</source>
-        <translation type="unfinished">请添加更多的账单！</translation>
+        <translation>请添加更多的账单！</translation>
     </message>
 </context>
 <context>
     <name>CategorySelector</name>
     <message>
         <source>Category</source>
-        <translation type="unfinished">类别</translation>
+        <translation>类别</translation>
     </message>
     <message>
         <source>Select Category</source>
-        <translation type="unfinished">选择类别</translation>
+        <translation>选择类别</translation>
     </message>
     <message>
         <source>Add Category</source>
-        <translation type="unfinished">添加类别</translation>
+        <translation>添加类别</translation>
     </message>
     <message>
         <source>New Category</source>
-        <translation type="unfinished">新类别</translation>
+        <translation>新类别</translation>
     </message>
     <message>
         <source>Category name</source>
-        <translation type="unfinished">类别名称</translation>
+        <translation>类别名称</translation>
     </message>
 </context>
 <context>
     <name>CoverPage</name>
     <message>
         <source>BudgetBook</source>
-        <translation type="unfinished">BudgetBook</translation>
+        <translation>BudgetBook</translation>
     </message>
 </context>
 <context>
     <name>CurrencySelector</name>
     <message>
         <source>Currency</source>
-        <translation type="unfinished">货币</translation>
+        <translation>货币</translation>
     </message>
     <message>
         <source>Select Currencyy</source>
-        <translation type="unfinished">选择货币</translation>
+        <translation>选择货币</translation>
     </message>
     <message>
         <source>Add Currency</source>
-        <translation type="unfinished">添加货币</translation>
+        <translation>添加货币</translation>
     </message>
     <message>
         <source>New Currency</source>
-        <translation type="unfinished">新货币</translation>
+        <translation>新货币</translation>
     </message>
     <message>
         <source>Currency symbol</source>
-        <translation type="unfinished">货币符号</translation>
+        <translation>货币符号</translation>
     </message>
 </context>
 <context>
     <name>CurrencySettings</name>
     <message>
         <source>Currency Settings</source>
-        <translation type="unfinished">货币设置</translation>
+        <translation>货币设置</translation>
     </message>
     <message>
         <source>Exchange rates</source>
-        <translation type="unfinished">汇率</translation>
+        <translation>汇率</translation>
     </message>
     <message>
         <source>to</source>
-        <translation type="unfinished">到</translation>
+        <translation>到</translation>
     </message>
     <message>
         <source>Exchange rate</source>
-        <translation type="unfinished">汇率</translation>
+        <translation>汇率</translation>
     </message>
 </context>
 <context>
     <name>Database</name>
     <message>
         <source>Other</source>
-        <translation type="unfinished">其它</translation>
+        <translation>其它</translation>
     </message>
     <message>
         <source>Shop with the given name does not exist or name is not unique!</source>
-        <translation type="unfinished">商店名不存在或重复</translation>
+        <translation>商店名不存在或重复</translation>
     </message>
     <message>
         <source>An item with the given name already exists!</source>
-        <translation type="unfinished">物品名已存在</translation>
+        <translation>物品名已存在</translation>
     </message>
     <message>
         <source>Super Market</source>
-        <translation type="unfinished">超市</translation>
+        <translation>超市</translation>
     </message>
     <message>
         <source>Category with the given name does not exist or category name is not unique!</source>
-        <translation type="unfinished">类别名不存在或重复</translation>
+        <translation>类别名不存在或重复</translation>
     </message>
     <message>
         <source>Shop type with the given name does not exist or shop type name is not unique!</source>
-        <translation type="unfinished">商店类型不存在或重复</translation>
+        <translation>商店类型不存在或重复</translation>
     </message>
     <message>
         <source>A tag with the given name already exists!</source>
-        <translation type="unfinished">标签名已存在</translation>
+        <translation>标签名已存在</translation>
     </message>
     <message>
         <source>Tag with the given name does not exist or tag name is not unique!</source>
-        <translation type="unfinished">标签名不存在或重复</translation>
+        <translation>标签名不存在或重复</translation>
     </message>
     <message>
         <source>Currency with the given symbol does not exist!</source>
-        <translation type="unfinished">符号对应的货币不存在</translation>
+        <translation>符号对应的货币不存在</translation>
     </message>
     <message>
         <source>Currency with the given id does not exist!</source>
-        <translation type="unfinished">id对应的货币不存在</translation>
+        <translation>id对应的货币不存在</translation>
     </message>
 </context>
 <context>
     <name>FirstPage</name>
     <message>
         <source>BudgetBook</source>
-        <translation type="unfinished">BudgetBook</translation>
+        <translation>BudgetBook</translation>
     </message>
     <message>
         <source>Add Bill</source>
-        <translation type="unfinished">添加账单</translation>
+        <translation>添加账单</translation>
     </message>
     <message>
         <source>Settings</source>
-        <translation type="unfinished">设置</translation>
+        <translation>设置</translation>
     </message>
     <message>
         <source>February statistics</source>
-        <translation type="unfinished">二月统计数据</translation>
+        <translation>二月统计数据</translation>
     </message>
 </context>
 <context>
     <name>Settings</name>
     <message>
         <source>Settings</source>
-        <translation type="unfinished">设置</translation>
+        <translation>设置</translation>
     </message>
     <message>
         <source>Primary currency</source>
-        <translation type="unfinished">主要货币</translation>
+        <translation>主要货币</translation>
     </message>
 </context>
 <context>
     <name>ShopSelector</name>
     <message>
         <source>Add Shop</source>
-        <translation type="unfinished">添加商店</translation>
+        <translation>添加商店</translation>
     </message>
     <message>
         <source>Shop</source>
-        <translation type="unfinished">商店</translation>
+        <translation>商店</translation>
     </message>
     <message>
         <source>Select Shop</source>
-        <translation type="unfinished">选择商店</translation>
+        <translation>选择商店</translation>
     </message>
     <message>
         <source>New Shop</source>
-        <translation type="unfinished">新商店</translation>
+        <translation>新商店</translation>
     </message>
     <message>
         <source>Shop name</source>
-        <translation type="unfinished">商店名</translation>
+        <translation>商店名</translation>
     </message>
 </context>
 <context>
     <name>ShopTypeSelector</name>
     <message>
         <source>Shop Type</source>
-        <translation type="unfinished">商店类型</translation>
+        <translation>商店类型</translation>
     </message>
     <message>
         <source>Select Shop Type</source>
-        <translation type="unfinished">选择商店类型</translation>
+        <translation>选择商店类型</translation>
     </message>
     <message>
         <source>Add Shop Type</source>
-        <translation type="unfinished">添加商店类型</translation>
+        <translation>添加商店类型</translation>
     </message>
     <message>
         <source>New Shop Type</source>
-        <translation type="unfinished">新商店类型</translation>
+        <translation>新商店类型</translation>
     </message>
     <message>
         <source>Shop Type name</source>
-        <translation type="unfinished">商店类型名称</translation>
+        <translation>商店类型名称</translation>
     </message>
 </context>
 <context>
     <name>Statistics</name>
     <message>
         <source>Statistics</source>
-        <translation type="unfinished">统计</translation>
+        <translation>统计</translation>
     </message>
     <message>
         <source>Browse Bills</source>
-        <translation type="unfinished">浏览账单</translation>
+        <translation>浏览账单</translation>
     </message>
     <message>
         <source>January statistics</source>
-        <translation type="unfinished">一月统计数据</translation>
+        <translation>一月统计数据</translation>
     </message>
     <message>
         <source>Overall statistics</source>
-        <translation type="unfinished">所有统计数据</translation>
+        <translation>所有统计数据</translation>
     </message>
 </context>
 <context>
     <name>TagSelector</name>
     <message>
         <source>Add tag ...</source>
-        <translation type="unfinished">添加标签…</translation>
+        <translation>添加标签…</translation>
     </message>
 </context>
 </TS>
